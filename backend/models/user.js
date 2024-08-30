6ec4eaee4b961cdf7637473e097b1e4d720d4b78
AyushKhamar <ayushkhamar8@gmail.com>
2024-08-30 23:09:44 +0530

    feat: add user model

---
+++ backend/models/user.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true, minLength: 6 },
+    cartItems: [
+      {
+        quantity: { type: Number, default: 1 },
+        product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      },
+    ],
+    role: { type: String, enum: ["customer", "admin"], default: "customer" },
+  },
+  { timestamps: true }
+);
+
+//pre save hook to hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
